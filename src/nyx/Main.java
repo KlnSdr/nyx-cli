8340a6ff86f969191618e31b3838bf1837499d53
KlnSdr <monsieurk1209@gmail.com>
2025-05-02 23:00:51 +0200

    add pipeline def

--- src/nyx/Main.java
+++ src/nyx/Main.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 
 public class Main {
     private static final Logger LOGGER = new Logger(Main.class, true);
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.4-snapshot";
 
     public static void main(String[] args) {
         Logger.setMaxLogLevel(LogLevel.INFO);
